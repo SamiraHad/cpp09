83e30abb49a839a6e6ad9c77478fdf1fb0be1512
hsamira <hsamira@f1br1s4.paris.42.school>
2026-06-08 17:40:34 +0200

    new

--- ex02/SRC/PmergeMe.cpp
+++ ex02/SRC/PmergeMe.cpp
@@ -6,11 +6,9 @@
 /*   By: hsamira <hsamira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 12:11:52 by hsamira           #+#    #+#             */
-/*   Updated: 2026/06/04 12:06:32 by hsamira          ###   ########.fr       */
+/*   Updated: 2026/06/08 17:14:54 by hsamira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "PmergeMe.hpp"
 
 /*Le but de l'exercice est d'implémenter l'algorithme Ford–Johnson.
 
@@ -20,11 +18,12 @@ Je dois l'implémenter une fois avec std::vector et une fois avec std::deque pui
 
 
 
-std::vector<size_t> PmergeMe::Jacobsthal(size_t n) 
-/*   Cette fonction génère les nombres de Jacobsthal.
-     Ils servent à déterminer l'ordre optimal d'insertion des losers dans Ford–Johnson. 
+#include "PmergeMe.hpp"
 
-     La suite de Jacobsthal J0 = 0, J1 = 1, Jn = Jn-1 + 2 × Jn-2    pour n ≥ 2*/
+std::vector<size_t> PmergeMe::Jacobsthal(size_t n) 
+/*  Cette fonction génère seulement la suite/les nombres de Jacobsthal qui est definit par:  J0 = 0,
+                                                                        J1 = 1, 
+                                                                        Jn = Jn-1 + 2 × Jn-2    pour n ≥ 2*/
 {
     std::vector<size_t> jacob;
 
@@ -37,18 +36,21 @@ std::vector<size_t> PmergeMe::Jacobsthal(size_t n)
         size_t size = jacob.size();
         jacob.push_back(jacob[size - 1] + 2 * jacob[size - 2]);
     }
-    return jacob; // retourne Suite obtenue : 1, 3, 5, 11, 21, 43, ...   0, 1 sont ignorés.
+    return jacob; // retourne Suite obtenue : 1, 3, 5, 11, 21, 43, ...   0, 1 sont ignorés car ils ne sont pas utiles pour pour construire l'ordre d'insertion.
 }
 
 std::vector<size_t> PmergeMe::getJacobSthalOrder(size_t n)
-/*  Cette fonction transforme la suite de Jacobsthal en ordre d'insertion.
-    Par exemple pour 5 losers : 0 2 1 4 3
-    Cela indique quel loser doit être inséré en premier, puis en deuxième, etc.*/
+/*  Cette fonction transforme la suite de Jacobsthal en ordre d'insertion
+
+    Cela indique quel loser doit être inséré en premier, puis en deuxième, etc.
+    
+    exemple: loserChain     :   index : 0 1 2 3 4
+                                        A B C D E */
 {
     std::vector<size_t> order;
 
-    if (n == 0)
-        return order;
+    if (n == 0) //S'il n'y a aucun loser,
+        return order; //il n'y a aucun ordre d'insertion à construire donc return un vecteur vide..
 
     order.push_back(0);
 
@@ -195,20 +197,27 @@ void PmergeMe::printAfterDeque() const
 
 double PmergeMe::getVectTime() const
 {
-    return(_vectTime);
+    return(_vectTime); //retourne  la durée du tri de vector
 }
 
 double PmergeMe::getDeqTime() const
 {
-    return(_deqTime);
+    return(_deqTime); //retourne  la durée du tri de deque
 }
 
 size_t PmergeMe::getVectSize() const
 {
-    return(_vect.size());
+    return(_vect.size()); //C'est un getter qui retourne le nombre d'éléments.
 }
 
 size_t PmergeMe::getDeqSize() const
 {
     return(_deq.size());
 }
+
+
+/*lower_bound ne compare pas avec tous les éléments.
+
+Il compare avec le milieu,
+puis le milieu de la moitié restante,
+puis encore le milieu : O(log n)*/
